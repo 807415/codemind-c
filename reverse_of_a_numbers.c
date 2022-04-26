774740cc96c84634c41a195ef2925667a34af593
Chandrika <97424826+807415@users.noreply.github.com>
2022-04-26 18:50:36 +0530

    #include<stdio.h> int main() {     int n,r,sum=0;     scanf("%d",&n);     while(n>0)     {         r=n%10;         sum=sum*10+r;         n=n/10;     }     printf("%d ",sum);     return 0; }

--- reverse_of_a_numbers.c
+++ reverse_of_a_numbers.c
@@ -5,9 +5,9 @@ int main()
     scanf("%d",&n);
     while(n>0)
     {
-      r=n%10;
-      sum=sum*10+r;
-      n=n/10;
+        r=n%10;
+        sum=sum*10+r;
+        n=n/10;
     }
     printf("%d ",sum);
     return 0;
